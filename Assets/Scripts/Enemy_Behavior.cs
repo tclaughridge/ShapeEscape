4c149f1b8aeec60f7df4b492ed5213d6e35f25f5
tmc2hp <tmc2hp@virginia.edu>
2024-04-24 22:31:13 -0400

    Level 6
    Basic level involving an enemy

--- Assets/Scripts/Enemy_Behavior.cs
+++ Assets/Scripts/Enemy_Behavior.cs
@@ -3,28 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Enemy_Behavior : MonoBehaviour
 {
-
-    // This has not been tested yet but I plan to test this in a bit
-
-    public GameObject player;   // Player game object
+    private Transform target;
     public Rigidbody2D enemy_rb;    // Rigid body of the enemy
 
     public float Enemy_Speed = 1f;  // Speed of the enemy (needs to be tweaked)
     private float target_xpos;      // variable to store the x position of the player
     private int move_direction = 1; // sets the enemy's move direction towards the player. If -1, it moves left and if 1, it moves right
 
+    private void Awake()
+    {
+        enemy_rb = GetComponent<Rigidbody2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // identify the target
+        target = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Updates the target variable
-        target_xpos = player.transform.position.x;
+        if (target != null)
+        {
+            target_xpos = target.position.x;
+        }
 
         // Sets the move direction by comparing the enemy position to the player's
         if (target_xpos < enemy_rb.position.x)
